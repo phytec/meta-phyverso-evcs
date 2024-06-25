27b6701d751aa7c454ecfad9dd25410e81a55a59
Alexander Bauer <a.bauer@phytec.de>
2024-06-25 10:19:04 +0200

    changes for testsystem image
    Signed-off-by: Alexander Bauer <a.bauer@phytec.de>

--- recipes-utils/cg5317-utils/files/cg5317-bringup.sh
+++ recipes-utils/cg5317-utils/files/cg5317-bringup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-ip addr add 192.168.5.11/24 dev seth0
-ip addr add 192.168.6.11/24 dev seth1
+ip addr add 192.168.5.12/24 dev seth0
+ip addr add 192.168.6.12/24 dev seth1
 ip link set seth0 up
 ip link set seth1 up
